a8331e488cfeaf0a18f3949deeecd250c24cde0a
Mher <99.p3rc3n7@gmail.com>
2025-04-11 00:06:01 +0400

    add navbar

--- src/projects/AlikhanyansProject/Main.jsx
+++ src/projects/AlikhanyansProject/Main.jsx
@@ -2,10 +2,12 @@ import Hero from "./Hero/Hero";
 import About from "./About/About";
 import Gallery from "./Gallery/Gallery";
 import Footer from "./Footer/Footer";
+import NavBar from "./NavBar/NavBar";
 
 const AlikhanyansProject = () => {
   return (
     <div>
+      <NavBar />
       <Hero />
       <About />
       <Gallery />
---
+++ src/projects/AlikhanyansProject/NavBar/NavBar.jsx
@@ -0,0 +1,31 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./NavBar.css";
+
+function NavBar() {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
+  return (
+    <header>
+      <h3>Project</h3>
+      <nav ref={navRef}>
+        <a href="/#">Home</a>
+        <a href="/#">About us</a>
+        <a href="/#">Gallery</a>
+        <a href="/#">About me</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
+}
+
+export default NavBar;
