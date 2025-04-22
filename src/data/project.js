849097aa5b91f36c1824535b81ff7b061e567b12
Mher <99.p3rc3n7@gmail.com>
2025-04-22 17:09:39 +0400

    add real pictures and data

--- src/data/project.js
+++ src/data/project.js
@@ -1,35 +1,109 @@
-import ArturAlikhanyan from "../assets/project/gallery/artur_alikhanyan.jpg";
-import BassGuitarist from "../assets/project/gallery/bass_guitarist.jpg";
-import Drummer from "../assets/project/gallery/drummer.jpg";
-import ElectricGuitarist from "../assets/project/gallery/electric_guitarist.jpg";
-import GuyWithSaxophone from "../assets/project/gallery/guy_with_instrument.jpg";
-import GuyWithIdkWhat from "../assets/project/gallery/guy_with_idk_what.jpg";
-import GuyWithZurna from "../assets/project/gallery/guy_with_zurna.jpg";
-import Keyboard from "../assets/project/gallery/keyboard.jpg";
+// import ArturAlikhanyan from "../assets/project/gallery/artur_alikhanyan.jpg";
+// import BassGuitarist from "../assets/project/gallery/bass_guitarist.jpg";
+// import Drummer from "../assets/project/gallery/drummer.jpg";
+// import ElectricGuitarist from "../assets/project/gallery/electric_guitarist.jpg";
+// import GuyWithSaxophone from "../assets/project/gallery/guy_with_instrument.jpg";
+// import GuyWithIdkWhat from "../assets/project/gallery/guy_with_idk_what.jpg";
+// import GuyWithZurna from "../assets/project/gallery/guy_with_zurna.jpg";
+// import Keyboard from "../assets/project/gallery/keyboard.jpg";
+import Aida_Aram from "../assets/project/gallery/aida_arami.JPG";
+import Anna_Harutyunyan from "../assets/project/gallery/anna_harutyunyan.PNG";
+import Anna_Navasardyan from "../assets/project/gallery/anna_navasardyan.jpg";
+import Anna_Sedrakyan from "../assets/project/gallery/anna_sedryakyan.jpg";
+import Aram_Hakobyan from "../assets/project/gallery/aram_hakobyan.jpg";
+import Areg_Karakhamyan from "../assets/project/gallery/areg_karakhamyan.jpg";
+import Arkadi_Chobanyan from "../assets/project/gallery/arkadi_chobanyan.jpg";
+import Arsen_Simonyan from "../assets/project/gallery/arsen_simonyan.jpg";
+import Artur_Knyazyan from "../assets/project/gallery/artur_knyazyan.jpeg";
+import Davit_Nersisyan from "../assets/project/gallery/davit_nersisyan.jpg";
+import Garnik_Gevorgyan from "../assets/project/gallery/garnik_gevorgyan.jpg";
+import Grisha_Simonyan from "../assets/project/gallery/grisha_simonyan.jpg";
+import Harutyun_Khachatryan from "../assets/project/gallery/harutyun_khachatryan.jpg";
+import Hayk_Grigoryan from "../assets/project/gallery/hayk_grigoryan.jpg";
+import Sergey_Areskin from "../assets/project/gallery/sergey_areskin.jpg";
+import Vahan_Urutyan from "../assets/project/gallery/vahan_urutyan.jpg";
 
 export const project = [
   {
-    img: ArturAlikhanyan,
+    fullName: "Aida Arami",
+    img: Aida_Aram,
+    instrument: "Երգչուհի",
   },
   {
-    img: BassGuitarist,
+    fullName: "Anna Harutyunyan",
+    img: Anna_Harutyunyan,
+    instrument: "Երգչուհի",
   },
   {
-    img: Drummer,
+    fullName: "Anna Navasardyan",
+    img: Anna_Navasardyan,
+    instrument: "Երգչուհի",
   },
   {
-    img: ElectricGuitarist,
+    fullName: "Anna Sedrakyan",
+    img: Anna_Sedrakyan,
+    instrument: "Երգչուհի",
   },
   {
-    img: GuyWithSaxophone,
+    fullName: "Aram Hakobyan",
+    img: Aram_Hakobyan,
+    instrument: "Դհոլ, թմբուկներ",
   },
   {
-    img: GuyWithIdkWhat,
+    fullName: "Areg Karakhamyan",
+    img: Areg_Karakhamyan,
+    instrument: "Սաքսաֆոն",
+  },
+
+  {
+    fullName: "Arsen Simonyan",
+    img: Arsen_Simonyan,
+    instrument: "Տրոմբոն",
+  },
+
+  {
+    fullName: "Grisha Simonyan",
+    img: Grisha_Simonyan,
+    instrument: "Սոլո կիթառ",
   },
   {
-    img: GuyWithZurna,
+    fullName: "Harutyun Khachatryan",
+    img: Harutyun_Khachatryan,
+    instrument: "Երգիչ",
   },
   {
-    img: Keyboard,
+    fullName: "Hayk Grigoryan",
+    img: Hayk_Grigoryan,
+    instrument: "Շեփոր",
+  },
+  {
+    fullName: "Sergey Areskin",
+    img: Sergey_Areskin,
+    instrument: "Բաս կիթառ",
+  },
+  {
+    fullName: "Vahan Urutyan",
+    img: Vahan_Urutyan,
+    instrument: "Կլարնետ, դուդուկ, պկու",
+  },
+  {
+    fullName: "Davit Nersisyan",
+    img: Davit_Nersisyan,
+    instrument: "Երգիչ",
+  },
+  {
+    fullName: "Garnik Gevorgyan",
+    img: Garnik_Gevorgyan,
+    instrument: "Երգիչ",
+  },
+  {
+    fullName: "Arkadi Chobanyan",
+    img: Arkadi_Chobanyan,
+    instrument: "Ստեղնաշարային գործիքներ",
+  },
+  {
+    fullName: "Artur Knyazyan",
+    img: Artur_Knyazyan,
+    instrument: "Հարվածային գործիքներ",
   },
 ];
--- src/projects/AlikhanyansProject/Gallery/Gallery.jsx
+++ src/projects/AlikhanyansProject/Gallery/Gallery.jsx
@@ -5,7 +5,7 @@ const Gallery = () => {
     <div className="bg-red-950 flex-col gap-30">
       <h1 className="text-[40px] text-white text-center pt-11">Gallery</h1>
 
-      <div className="flex flex-col md:grid md:grid-cols-2 gap-10 justify-center items-center bg-red-950 pt-16 pb-16">
+      <div className="flex flex-col md:grid md:grid-cols-3 gap-10 justify-center items-center bg-red-950 pt-16 pb-16">
         {project.map((project, index) => (
           <div
             key={index}
@@ -13,8 +13,18 @@ const Gallery = () => {
           >
             <div className="gallery-title flex items-center justify-center"></div>
             <div className="pictures-layout">
-              <div className="w-[330px] md:w-[500px]">
+              <div className="w-[330px] md:w-[400px]">
                 <img className="rounded-2xl" src={project.img} alt="" />
+                <div className="flex items-center justify-center">
+                  <div className="flex gap-5 items-center">
+                    <span className="text-white text-[20px]">
+                      {project.fullName}
+                    </span>
+                    <span className="text-white text-[20px]">
+                      {project.instrument}
+                    </span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
