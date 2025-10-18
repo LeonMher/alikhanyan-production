5723c9c74ab965fa2de5dc697b6b6e5cf926186c
LeonMher <99.p3rc3n7@gmail.com>
2025-10-18 10:44:19 +0400

    center the hero section items

--- src/projects/AlikhanyansProject/Hero/Hero.jsx
+++ src/projects/AlikhanyansProject/Hero/Hero.jsx
@@ -4,20 +4,21 @@ import { Button } from "@mui/material";
 const Hero = () => {
   return (
     <div className="flex flex-row items-center">
-      <div className="hero-container flex flex-row justify-center md:justify-start items-start p-[60px] md:p-[90px]">
-        <div className="flex flex-col items-center md:items-start justify-center gap-[50px] md:gap-[20px]">
-          <div className="text-[70px] font-bold flex flex-col">
-            <span>Alikhanyan's</span>{" "}
-            <span className="flex justify-center md:justify-start">
-              Project
-            </span>
+      <div className="hero-container flex items-around p-[40px] md:p-[90px]">
+        <div className="hero-inner w-full p-[50px] flex flex-col md:flex-row items-center md:items-center justify-between">
+          <div className="hero-left flex flex-col items-start">
+            <div className="text-[48px] md:text-[70px] font-bold leading-tight">
+              <div>Alikhanyan's</div>
+              <div>Project</div>
+            </div>
           </div>
-          <div className="flex justify-center text-[32px]">
-            Call us for events
+
+          <div className="hero-right flex flex-col items-center md:items-end gap-4">
+            <div className="text-[20px] md:text-[32px]">Call us for events</div>
+            <Button sx={{ width: "180px" }} variant="contained">
+              Call
+            </Button>
           </div>
-          <Button sx={{ width: "200px" }} variant="contained">
-            Call
-          </Button>
         </div>
       </div>
     </div>
